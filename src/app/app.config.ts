399ccbe9508ac79696e5f1d5493db1ce455cd600
Mateo Llerena <mateollerena40@gmail.com>
2026-01-08 15:50:52 -0500

    Agregando un loading

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync('noop'),
-    provideHttpClient(
-      withInterceptors([])
-    )
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }
   ],
 };
---
+++ src/app/shared/components/loading/loading.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { LoadingService } from '../../services/loading.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'shared-loading',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './loading.component.html',
+  styleUrl: './loading.component.css'
+})
+export class LoadingComponent {
+
+  constructor(public loadingService:LoadingService) {}
+
+}
